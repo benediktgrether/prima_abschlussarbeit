5c20815de66df15f2512d55d592f022b8fe00a86
Benedikt Grether <info@benediktgrether.de>
2020-02-11 17:42:41 +0100

    Sound

--- prima_endaufgabe_grether_benedikt/js/Sound.js
+++ prima_endaufgabe_grether_benedikt/js/Sound.js
@@ -11,23 +11,13 @@ var prima_endaufgabe_grether_benedikt;
             Sound.sounds[_id].play();
             console.log(Sound.sounds[_id]);
         }
-        static playAtmo(_delay = Sound.atmoDelay) {
-            Sound.play("beat" + Sound.atmoBeat);
-            // Sound.atmoBeat = (Sound.atmoBeat == 1) ? 2 : 1;
-            // Sound.atmoDelay = _delay;
-            // window.setTimeout(Sound.playAtmo);
-            // if (Sound.atmoDelay > 0)
-            //   window.setTimeout(Sound.playAtmo, Sound.atmoDelay * 1000);
-        }
         static playMusic() {
             Sound.sounds["beat1"].loop = true;
             Sound.sounds["beat1"].play();
             Sound.sounds["beat1"].volume = 0.2;
         }
     }
-    Sound.atmoDelay = 0;
     Sound.sounds = {};
-    Sound.atmoBeat = 1;
     prima_endaufgabe_grether_benedikt.Sound = Sound;
 })(prima_endaufgabe_grether_benedikt || (prima_endaufgabe_grether_benedikt = {}));
 //# sourceMappingURL=Sound.js.map
--- prima_endaufgabe_grether_benedikt/js/Sound.ts
+++ prima_endaufgabe_grether_benedikt/js/Sound.ts
@@ -4,9 +4,7 @@ namespace prima_endaufgabe_grether_benedikt {
     [id: string]: HTMLAudioElement;
   }
   export class Sound {
-    public static atmoDelay: number = 0;
     private static sounds: Sounds = {};
-    private static atmoBeat: number = 1;
 
     public static init(): void {
       let audioElements: NodeListOf<HTMLAudioElement> = document.querySelectorAll("audio");
@@ -19,14 +17,6 @@ namespace prima_endaufgabe_grether_benedikt {
       console.log(Sound.sounds[_id]);
     }
 
-    public static playAtmo(_delay: number = Sound.atmoDelay): void {
-      Sound.play("beat" + Sound.atmoBeat);
-      // Sound.atmoBeat = (Sound.atmoBeat == 1) ? 2 : 1;
-      // Sound.atmoDelay = _delay;
-      // window.setTimeout(Sound.playAtmo);
-      // if (Sound.atmoDelay > 0)
-      //   window.setTimeout(Sound.playAtmo, Sound.atmoDelay * 1000);
-    }
     public static playMusic(): void {
       Sound.sounds["beat1"].loop = true;
       Sound.sounds["beat1"].play();
